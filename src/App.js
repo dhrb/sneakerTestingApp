7ec83bd549e2e5483cee7e6eac7e117034188b70
hasskell <hasskell@hasskells-MacBook-Pro.local>
2023-01-07 17:42:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import logo from './assets/img/sneakers.png'
+import cart from './assets/img/cart.png'
+import logIn from './assets/img/logIn.png'
+import snek1 from './assets/img/1.png'
+import snek5 from './assets/img/2.png'
+import snek2 from './assets/img/3.png'
+import snek3 from './assets/img/4.png'
+import snek4 from './assets/img/5.png'
+import snek6 from './assets/img/6.png'
+import addCart from './assets/img/addCart.png'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapp">
+      <header className='headerWrap'>
+        <div className='header'>
+          <div className='logo'>
+            <div className='logotype'>
+              <img className='logoImg' alt='logoImg' src={logo}/>
+            </div>
+            <div className='name'>
+              <p className='logoName'>REACT SNEAKERS</p>
+              <p className='logoDescription'>Магазин кросівок</p>
+            </div>
+          </div>
+          <div className='cartBlock'>
+            <ul className='cart'>
+              <li className='cartItem'>
+                <span className='cartSum'>100uah</span>
+                <img alt='cart' className='cartImg' src={cart} />
+              </li>
+              <li className='logInItem'>
+                <img src={logIn} className='cartImg' alt='logIn'/>
+              </li>
+            </ul>
+          </div>
+        </div>
       </header>
+      <div className='content'>
+        <div className='nameContent'>
+          <h1>Кросівки</h1>
+        </div>
+        <div className='productCard'>
+          <div className='productItem'>
+            <img className='prodImg' src={snek6} alt='prodItem' />4
+            <span className='productName'>Найк шото там модне</span>
+            <div className='cardBottom'>
+              <span className='productPriceDesc'>Ціна: <b>2500uah</b></span>
+              <img src={addCart} alt='addcartitem' className='addCartImg' />
+            </div>
+          </div>
+          <div className='productItem'>
+            <img className='prodImg' src={snek1} alt='prodItem' />
+            <span className='productName'>Найк шото там модне</span>
+            <span className='productPriceDesc'>Ціна: <b>2500uah</b></span>
+            <span className='productPrice'>{props.productPrice}</span>
+          </div>
+          <div className='productItem'>
+            <img className='prodImg' src={snek2} alt='prodItem' />
+            <span className='productName'>Найк шото там модне</span>
+            <span className='productPriceDesc'>Ціна: <b>2500uah</b></span>
+            <span className='productPrice'>{props.productPrice}</span>
+          </div>
+          <div className='productItem'>
+            <img className='prodImg' src={snek3} alt='prodItem' />
+            <span className='productName'>Найк шото там модне</span>
+            <span className='productPriceDesc'>Ціна: <b>2500uah</b></span>
+            <span className='productPrice'>{props.productPrice}</span>
+          </div>
+          <div className='productItem'>
+            <img className='prodImg' src={snek4} alt='prodItem' />
+            <span className='productName'>Найк шото там модне</span>
+            <span className='productPriceDesc'>Ціна: <b>2500uah</b></span>
+            <span className='productPrice'>{props.productPrice}</span>
+          </div>
+          <div className='productItem'>
+            <img className='prodImg' src={snek5} alt='prodItem' />
+            <span className='productName'>Найк шото там модне</span>
+            <span className='productPriceDesc'>Ціна: <b>2500uah</b></span>
+            <span className='productPrice'>{props.productPrice}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
